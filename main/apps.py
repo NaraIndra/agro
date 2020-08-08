860bddc68b9400bf165acc2e9f21df2e15c0d4ab
NaraIndra <pindak.andrey@gmail.com>
2020-08-08 17:19:27 -0400

    rebuild to flask

--- main/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkingAlgoConfig(AppConfig):
-    name = 'main'
--- passenger_wsgi.py
+++
@@ -1,6 +0,0 @@
-import os, sys
-sys.path.insert(0, '/var/www/u1108756/data/www/avagro.fun/avagro')
-sys.path.insert(1, '/var/www/u1108756/data/djangoenv/lib/python3.7/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'avagro.settings'
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
